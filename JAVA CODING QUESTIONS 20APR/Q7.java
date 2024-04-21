e6f7e22105529e3620bafe7dd818331130013637
Suchit <suchit2409@gmail.com>
2024-04-21 10:23:21 +0530

    learning

--- JAVA CODING QUESTIONS 20APR/Q7.java
+++ JAVA CODING QUESTIONS 20APR/Q7.java
@@ -6,5 +6,6 @@ public class Q7 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hellow World");
+        System.out.println("my name is suchit");
     }
 }
